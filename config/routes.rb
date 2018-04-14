e2f4415ac11c08c7ffaffe293401443fe0a68607
ingridwong0715 <ingrid830715@gmail.com>
2018-04-14 16:17:26 -0400

    further configurations in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+  get '/home', to: 'users#show' #show page? Or like a user home page?
+
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' },
+  path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register' }
+
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
 
   root to: 'welcome#welcome'
 
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-
-  get '/home', to: 'users#show' #show page? Or like a user home page?
 
   resources :projects do
-    resources :tasks # nest under projects?!
+    resources :tasks 
   end
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
 end
