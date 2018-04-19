85ab8e58471dacc1a9f15e69f63b18143672e4f4
ingridwong0715 <ingrid830715@gmail.com>
2018-04-19 19:28:28 -0400

    created a separate layout for home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register' }
 
 
-
   authenticated :user do
     root 'users#show', as: :authenticated_root
   end
