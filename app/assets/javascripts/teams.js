19b81ea84631af7cd03751374311d9de570d7429
ingridwong0715 <ingrid830715@gmail.com>
2018-05-09 12:20:16 -0400

    refactored team#show projects index using handlebars partial

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,34 +1,35 @@
 $(function(){
-
+  handlebarsSetup();
   $("a.view_projects").on('click', function(e){
-    var source   = document.getElementById("project-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var context = {title: "My New Post", body: "This is my first post!"};
-    var html    = template(context);
+      e.preventDefault();
 
-    var teamId = $(this).data("id")
-    var tbody = $("div.projects tbody")
+      var teamId = $(this).data("id")
 
-    if (this.innerText=="Hide Projects"){
+      if (this.innerText=="Hide Projects"){
 
-      $("div.projects tbody").empty();
-      this.innerText = "View Projects"
+        $("div.projects table").empty();
+        this.innerText = "View Projects"
+      } else {
 
-    } else {
+        $.get(this.href, function(data) {
 
+          var projectsContext = []
+          data.forEach(function(project){
+            projectsContext.push({team_id: teamId, id: project["id"], name: project["name"], description: project["description"]})
+          });
 
-      $.get(this.href, function( data ) {
-        data.forEach(function(project){
-          var due_date  = new Date(project.due_date).toString();
-          var context = {team_id: teamId, id: project["id"], name: project["name"], description: project["description"], due_date: due_date}
-          var html    = template(context);
-          tbody.append(html);
-        })
-      });
+          var source   = document.getElementById("projects-table-template").innerHTML;
+          var template = Handlebars.compile(source);
 
-      this.innerText = "Hide Projects"
-    }
-    e.preventDefault();
-  })
+          var html = template(projectsContext);
+          $("div.projects").append(html);
+        });
+        this.innerText="Hide Projects"
 
+      } // end else
+    });
 })
+
+function handlebarsSetup(){
+  Handlebars.registerPartial('projectPartial', document.getElementById('project-template-partial').innerHTML)
+}
