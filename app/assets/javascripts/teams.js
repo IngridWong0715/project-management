0dc54ec29df0a03969853b4f21bd25c8104fed21
ingridwong0715 <ingrid830715@gmail.com>
2018-05-10 10:45:57 -0400

    used delegated event on 'div.projects' to invoke a handler on each dynamically created project link

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -3,6 +3,7 @@ $(function(){
     handlebarsSetup();
   }
 
+  // Refactor into a displayProject function
   $("a.view_projects").on('click', function(e){
       e.preventDefault();
 
@@ -23,10 +24,18 @@ $(function(){
           $("div.projects").append(html);
         });
         this.innerText="Hide Projects"
-
       }
     });
-})
+    //END OF REFACTORING NEEDED
+
+    // targeting <a class="project-link"> which is dynamically created by the displayProject function
+    // ISSUE: “ Event handlers are bound only to the currently selected elements; they must exist on the page at the time your code makes the call to .on().”
+    // since .on() is called at document ready, and the <a> tag is added dynamically later. Doesn't work
+    $('div.projects').on('click', '.project-link', function(e){
+        alert("YOU CLICKED!")
+      e.preventDefault();
+    })
+});
 
 function handlebarsSetup(){
   Handlebars.registerPartial('projectPartial', document.getElementById('project-partial-template').innerHTML)
