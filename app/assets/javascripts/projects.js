6809c380b283cbd69aab45006416e5b842f746d6
ingridwong0715 <ingrid830715@gmail.com>
2018-05-10 14:37:09 -0400

    working project#show with tasks index table: all using just one ajax request to projects/project_id

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,27 +2,27 @@ $(function(){
   if (document.querySelector("#task-partial-template")){
     projectHandlebarsSetup();
   }
-  $("a.view-tasks").on('click', function(e){
+  $("a.view-project-link").on('click', function(e){
 
     e.preventDefault();
     var projectId = $(this).data('id');
 
-      if (this.innerText=='Hide Tasks') {
-        $('div.tasks table').empty();
-        this.innerText = 'View Tasks'
-      } else {
+
         $.get(this.href, function(data){
-          var source   = document.getElementById("tasks-table-template").innerHTML;
+
+          var source   = document.getElementById("project-template").innerHTML;
           var template = Handlebars.compile(source);
           var html = template(data);
-          $('div.tasks').append(html);
-        });
-        this.innerText = "Hide Tasks"
+          debugger;
 
-      }
+          $(document.body).append(html);
+        }, 'json');
+
+
+
   });
 
-  $('div.tasks').on('click', '.task-link', function(e){
+  $(document.body).on('click', '.task-link', function(e){
     e.preventDefault();
     var source   = document.getElementById("task-show-template").innerHTML;
     var template = Handlebars.compile(source);
