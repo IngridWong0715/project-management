d92d7cf20a935c58e69ecf43cf4af37312074bdc
ingridwong0715 <ingrid830715@gmail.com>
2018-05-11 14:38:54 -0400

    refactored getSurroundingTask into one function with a position parameter

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -5,87 +5,44 @@ $(function(){
 
   addEventListeners();
 
-
-
-  // FUNCTION//Get and render the next task in a show page
-  $('div.task-box').on('click', '.next-task', function(e){
-    e.preventDefault();
-
-    // IS THERE A BETTER WAY TO GET PROJECT AND TASK?
-    var project = $(this).data('project')
-    var task = $('div.task-box').data('task')
-
-    // fetch next task
-      $.get(`http://localhost:3000/projects/${project}/tasks/${task}/surrounding_tasks`, function(data){
-
-        var nextTask = data['next']
-        if (nextTask) { //if the current task is not the last task
-          $.get(`http://localhost:3000/projects/${project}/tasks/${nextTask}`, function(data){
-            //repeated code!!!
-            $('div.task-box').empty();
-              var source   = document.getElementById("task-show-template").innerHTML;
-              var template = Handlebars.compile(source);
-              var html = template(data);
-              $('div.task-box').html(html);
-              //end of repeated code
-
-              // newReferenceTask is the current task, that is then passed into task-box
-              //to reference for next cycle of previous/next request
-              var taskId = this.url.split('/')
-              var newReferenceTask = taskId[taskId.length-1]
-              $('div.task-box').data('task', newReferenceTask)
-
-          },'json')
-
-        } else {
-          alert("You're viewing the last task");
-        }
-
-      }, 'json')
-  });//FUNCTION
-
-
-  // FUNCTION//Get and render the previous task in a show page
-  $('div.task-box').on('click', '.previous-task', function(e){
-    e.preventDefault();
-
-    // IS THERE A BETTER WAY TO GET PROJECT AND TASK?
-    var project = $(this).data('project')
-    var task = $('div.task-box').data('task')
-
-    // fetch next task
-      $.get(`http://localhost:3000/projects/${project}/tasks/${task}/surrounding_tasks`, function(data){
-
-        var previousTask = data['previous']
-        if (previousTask) {
-          $.get(`http://localhost:3000/projects/${project}/tasks/${previousTask}`, function(data){
-            //repeated code!!!
-            $('div.task-box').empty();
-              var source   = document.getElementById("task-show-template").innerHTML;
-              var template = Handlebars.compile(source);
-              var html = template(data);
-              $('div.task-box').html(html);
-              //end of repeated code
-
-              // newReferenceTask is the current task, that is then passed into task-box
-              //to reference for next cycle of previous/next request
-              var taskId = this.url.split('/')
-              var newReferenceTask = taskId[taskId.length-1]
-
-              $('div.task-box').data('task', newReferenceTask)
-
-          },'json')
-
-        } else {
-          alert("You're viewing the first task");
-        }
-
-      }, 'json')
-  });//FUNCTION
-
-
 });// END OF DOCUMENT READY
 
+function getSurroundingTask(position){
+  // IS THERE A BETTER WAY TO GET PROJECT AND TASK?
+  var project = $(this).data('project')
+  var task = $('div.task-box').data('task')
+
+  // fetch next task
+    $.get(`http://localhost:3000/projects/${project}/tasks/${task}/surrounding_tasks`, function(data){
+
+      var previousTask = data[position]
+      if (previousTask) {
+        $.get(`http://localhost:3000/projects/${project}/tasks/${previousTask}`, function(data){
+          //repeated code!!!
+          $('div.task-box').empty();
+            var source   = document.getElementById("task-show-template").innerHTML;
+            var template = Handlebars.compile(source);
+            var html = template(data);
+            $('div.task-box').html(html);
+            //end of repeated code
+
+            // newReferenceTask is the current task, that is then passed into task-box
+            //to reference for next cycle of previous/next request
+            var taskId = this.url.split('/')
+            var newReferenceTask = taskId[taskId.length-1]
+
+            $('div.task-box').data('task', newReferenceTask)
+
+        },'json')
+
+      } else {
+        let index = (position == 'previous') ? 'first' : 'last'
+        alert(`You're viewing the ${index} task`);
+      }
+
+    }, 'json')
+
+}
 
 function addEventListeners(){
   $('form.new_task').on('submit', function(e){
@@ -96,6 +53,17 @@ function addEventListeners(){
   $('div.task-box').on('click', '.task-link', function(e){
     e.preventDefault();
     loadTaskShowPage(this);
+  });
+
+  $('div.task-box').on('click', '.next-task', function(e){
+    e.preventDefault();
+    getSurroundingTask('next')
+  });
+
+
+  $('div.task-box').on('click', '.previous-task', function(e){
+    e.preventDefault();
+    getSurroundingTask('previous')
   });
 }
 
@@ -113,26 +81,6 @@ function addEventListeners(){
     $('div.task-box').data('task', $(task).data('task'))
 
   }
-
-
-  // FUNCTION//Task show page when clicked on each task name in the table
-  // $('div.task-box').on('click', '.task-link', function(e){
-  //   e.preventDefault();
-  //
-  //   $.get(this.href, function(data){
-  //
-  //     $('div.task-box').empty();
-  //       var source   = document.getElementById("task-show-template").innerHTML;
-  //       var template = Handlebars.compile(source);
-  //       var html = template(data);
-  //       $('div.task-box').html(html);
-  //   }, 'json');
-  //   // ASK!!!(ideally, want to add these 2 values to the dynamically created next and previous links: but can't because it's ASYNC)
-  //   // so save them to the div.task-box
-  //   $('div.task-box').data('task', $(this).data('task'))
-  //
-  // })// FUNCTION
-
 
 function createNewTask(task){
   $.post($(task).attr('action'), $(task).serialize(), function(data){
