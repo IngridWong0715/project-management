f5225935f905360400bbc418c3433bc1a9e17005
ingridwong0715 <ingrid830715@gmail.com>
2018-05-12 14:51:42 -0400

    refactored using task prototype methods

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -7,9 +7,6 @@ $(function(){
 
 });// END OF DOCUMENT READY
 
-
-//TASK OBJECT: CONSTRUCTOR FUNCTION
-
 class Task{
   constructor(attributes){
     this.id = attributes.id;
@@ -18,6 +15,9 @@ class Task{
     this.project_id = attributes.project_id;
     this.due_date = attributes.due_date;
     this.complete = attributes.complete;
+    this._next = 0; //defaulting to 0 first... fix later
+    this._previous = 0;//defaulting to 0 first
+    //this.set_surrounding_tasks();
   }
 
   formatDisplay(){
@@ -31,12 +31,61 @@ class Task{
     return formatted;
   }
 
+  formatShow(){
+    let formatted =
+    `<div class="container">
+      <div class="row">
+         <div class="col formatted_show">
+           <h3 class="text-center">${this.name} </h3>
+           <a href="#" class="previous-task" data-project="${this.project_id}">Previous Task | </a>
+           <a href="#" class="next-task" data-project="${this.project_id}">Next Task</a>
 
-}
+           <div class="row">
+             <div class="col-8 col-sm-6 description_box">
+               ${this.description}
+             </div>
+             <div class="col-8 col-sm-6">
+                Due: ${this.due_date}<br>
+                Create a checklist<br>
+                Add attachment <br>
+                Have a "complete task" button <br>
+                <div class="right_bottom_link">
+                  <a href="/projects/${this.project_id}/tasks/${this.id}/edit">Edit Task</a>
+                  <a href="/projects/${this.project_id}/tasks/${this.id}">Delete Task Task</a>
+                </div>
+             </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+
+    return formatted;
+  }
+
+  //NOT WORKING
+  set next(newNext){
+    this._next = newNext; // look into this _ convention:
+  }
+  set previous(newPrevious){
+    this._previous = newPrevious;
+  }
+
+  set_surrounding_tasks(){//SETS TASK.PREVIOUS_TASK_ID & TAKS.NEXT_TASK_ID
+    var task = this;
+    $.get(`http://localhost:3000/projects/${this.project_id}/tasks/${this.id}/surrounding_tasks`, function(data){
+      task.next = data['next']
+      task.previous = data['previous']
+    });
+  }
+  //NOT WORKING
+
+
+} // END CLASS TASK
 
 function createNewTask(task){
   $.post($(task).attr('action'), $(task).serialize(), function(data){
     let task = new Task(data);
+    task.set_surrounding_tasks();
     $('table.tasks_table tbody').append(task.formatDisplay());
   }, 'json')
 }
@@ -47,19 +96,16 @@ function getSurroundingTask(position){
   let project = $(this).data('project')
   let task = $('div.task-box').data('task')
 
-  // fetch next task
+  // fetch ${position} task
     $.get(`http://localhost:3000/projects/${project}/tasks/${task}/surrounding_tasks`, function(data){
 
       let previousTask = data[position]
       if (previousTask) {
         $.get(`http://localhost:3000/projects/${project}/tasks/${previousTask}`, function(data){
-          //repeated code!!!
-          $('div.task-box').empty();
-            let source   = document.getElementById("task-show-template").innerHTML;
-            let template = Handlebars.compile(source);
-            let html = template(data);
-            $('div.task-box').html(html);
-            //end of repeated code
+
+          let task = new Task(data);
+          $('div.task-box').html(task.formatShow());
+
 
             // newReferenceTask is the current task, that is then passed into task-box
             //to reference for next cycle of previous/next request
@@ -74,9 +120,7 @@ function getSurroundingTask(position){
         let index = (position == 'previous') ? 'first' : 'last'
         alert(`You're viewing the ${index} task`);
       }
-
     }, 'json')
-
 }
 
 function addEventListeners(){
